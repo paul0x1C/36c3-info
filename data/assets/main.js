d8d91a4c46a2f2368b436a5365712b9815450c3c
Flemming Petersen <mail@flemming-petersen.de>
2019-12-12 08:27:44 +0100

    Make it to pwa

---
+++ data/assets/main.js
@@ -0,0 +1,25 @@
+var cacheName = '36c3.info';
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/assets/main.css'
+];
+self.addEventListener('install', function (e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true }).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
