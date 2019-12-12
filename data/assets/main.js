37209171b78c9691a4557d517634af2c466effd8
Flemming Petersen <mail@flemming-petersen.de>
2019-12-12 21:12:04 +0100

    New try with service worker

--- data/assets/main.js
+++ data/assets/main.js
@@ -1,39 +1,8 @@
-self.addEventListener('install', function (event) {
-  event.waitUntil(
-    caches.open('v1').then(function (cache) {
-      return cache.addAll([
-        '/assets/GitHub-Mark-Light-32px.png',
-        '/assets/icon.png',
-        '/assets/logo.svg',
-        '/index.html',
-        '/assets/main.js',
-        '/assets/main.css',
-        '/favicon.ico'
-      ]);
-    })
-  );
-});
+window.onload = () => {
+  'use strict';
 
-self.addEventListener('fetch', function (event) {
-  event.respondWith(caches.match(event.request).then(function (response) {
-    // caches.match() always resolves
-    // but in case of success response will have value
-    if (response !== undefined) {
-      return response;
-    } else {
-      return fetch(event.request).then(function (response) {
-        // response may be used only once
-        // we need to save clone to put one copy in cache
-        // and serve second one
-        let responseClone = response.clone();
-
-        caches.open('v1').then(function (cache) {
-          cache.put(event.request, responseClone);
-        });
-        return response;
-      }).catch(function () {
-        return fetch(event.request);
-      });
-    }
-  }));
-});
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/assets/sw.js');
+  }
+}
