6c94fa3b015fc808ec2aae5f86f86271ab2a00a5
Flemming Petersen <mail@flemming-petersen.de>
2019-12-12 21:05:06 +0100

    Try new service worker

--- data/assets/main.js
+++ data/assets/main.js
@@ -1,25 +1,39 @@
-var cacheName = '36c3.info';
-var filesToCache = [
-  '/',
-  '/index.html',
-  '/assets/main.css'
-];
-self.addEventListener('install', function (e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function (cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open('v1').then(function (cache) {
+      return cache.addAll([
+        '/assets/GitHub-Mark-Light-32px.png',
+        '/assets/icon.png',
+        '/assets/logo.svg',
+        '/index.html',
+        '/assets/main.js',
+        '/assets/main.css',
+        '/favicon.ico'
+      ]);
     })
   );
 });
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
-});
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request, { ignoreSearch: true }).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(caches.match(event.request).then(function (response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+
+        caches.open('v1').then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        return fetch(event.request);
+      });
+    }
+  }));
 });
